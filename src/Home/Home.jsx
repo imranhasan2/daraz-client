935fbff6bc30b25c4995708e6665423f46f425e1
imranhasan2 <imranhasan030303@gmail.com>
2024-08-16 20:04:47 +0600

    first commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+
+
+
+const Home = () => {
+    return (
+        <div>
+        <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className=''>
+            <div className="navbar mx-auto bg-[#F85606] flex  ">
+                <div className="flex-none ">
+                    
+                        <img src="https://lzd-img-global.slatic.net/us/domino/3b870cb043c7f8a9741cbf66329e294e.png" alt="" />
+                    
+                </div>
+                
+                <div className="flex-none">
+                    <button className="btn btn-square btn-ghost">
+                       
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
