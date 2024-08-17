2d1e4d341f01df33a38e7f6dc2e945b759480e8c
imranhasan2 <imranhasan030303@gmail.com>
2024-08-17 16:50:56 +0600

    added firebase

---
+++ src/Register.jsx
@@ -0,0 +1,99 @@
+import { useContext } from "react";
+import { AuthContext } from "./AuthProvider"
+import Swal from 'sweetalert2'
+import { Link } from "react-router-dom";
+
+
+const Register = () => {
+
+    const { createUser } = useContext(AuthContext)
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+        const form = e.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+
+        if(password.length < 6){
+           return Swal.fire({
+                title: 'error',
+                text: 'password must be 6 character',
+                icon: 'error',
+                confirmButtonText: 'ok'
+              })
+        }
+        if(!/[A-Z]/.test(password)){
+           return Swal.fire({
+                title: 'error',
+                text: 'Password contains at least one uppercase letter',
+                icon: 'error',
+                confirmButtonText: 'ok'
+              })
+        }
+        if(!/[a-z]/.test(password)){
+           return Swal.fire({
+                title: 'error',
+                text: 'Password contains at least one lowercase letter',
+                icon: 'error',
+                confirmButtonText: 'ok'
+              })
+        }
+
+
+
+        console.log(email,password)
+
+
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user)
+                .then(res => res.json())
+                .then (data => console.log(data))
+                Swal.fire({
+                    title: 'congrats',
+                    text: 'you have successfully registered',
+                    icon: 'success',
+                    confirmButtonText: 'ok'
+                  })
+            })
+            .catch(error => console.error(error))
+
+
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center ">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                        <p>Already have an Account? <span><Link to="/login" className="text-xl text-blue-400">Please Login</Link></span></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
--- src/main.jsx
+++ src/main.jsx
@@ -13,27 +13,30 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import AuthProvider from './AuthProvider.jsx';
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<Main></Main>,
-    children:[{
-      path:'/',
-      element:<Home></Home>
+    element: <Main></Main>,
+    children: [{
+      path: '/',
+      element: <Home></Home>
     }]
-    
+
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
-</StrictMode>
-  
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+
 )
